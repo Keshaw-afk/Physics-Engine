512a6e277b3f6b40b8dd4d6391f90130e977422c
Keshaw Tiwari <dimethylcadmium100@gmail.com>
2025-06-10 16:35:28 +0530

    Revert "Done with 3rd chapter"
    This reverts commit e73740b3df0d21c71f6ca621f1225cef4a1f858d.

--- build/CMakeFiles/cyclone.dir/physics/src/particle.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/cyclone.dir/physics/src/particle.cpp.o: \
- /home/graduate/Development/hobby/EXPMENT_PHYSICS_ENGINE/physics/src/particle.cpp \
- /usr/include/stdc-predef.h
--- include/cyclone/precision.h
+++ include/cyclone/precision.h
@@ -2,8 +2,6 @@
 
 #define real_sqrt sqrtf
 
-#define real_pow powf
-
 namespace cyclone {
     typedef float real;
     
